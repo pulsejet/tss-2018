86a2f21a39dc017a4d9fcd4fb2b9b47872fb90c7
Varun Patil <radialapps@gmail.com>
2018-06-13 00:55:24 +0530

    Add files for P4

---
+++ got-dict.py
@@ -0,0 +1,6 @@
+everyone = [
+    {'name': 'Daenerys', 'house': 'Targaryen', 'home': 'Dragonstone?'},
+    {'name': 'Arya', 'house': 'Stark', 'home': 'Winterfell'},
+    {'name': 'Cersei', 'house': 'Lannister', 'home': "King's Landing"},
+    {'name': 'Yara', 'house': 'Greyjoy', 'home': 'Iron Islands'}
+]
